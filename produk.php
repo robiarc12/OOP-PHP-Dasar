82223f5cf36de647cce3fa11a944fcec05e403d5
robiarc12 <robifirmansyah096@gmail.com>
2025-04-30 00:29:08 +0800

    membuat dan menambahkan properti, untuk method di bahas di pertemuan selanjutnya

--- produk.php
+++ produk.php
@@ -1,9 +1,32 @@
 <?php
-class produk
+class Produk
 {
+    // Ini adalaha sebuah Properti
+    public $judul = "Judul",
+    $penulis = "penulis",
+    $penerbit = "penerbit",
+    $harga = 0;
+
+
+    public function sayHello()
+    {
+        return "hello World";
+    }
 
 }
 
+
+// ini bagaimana cara kita memanggil class yang sudah kita buat
 $produk1 = new Produk();
+
+// cara memanggil dan menimpa isi default pada sebuah class
+$produk1->judul = "ill Principe";
+$produk1->penulis = "Machieveli";
+
 $produk2 = new Produk();
+$produk2->statusPembelian = "Pre-Order";
+var_dump($produk2);
+
+
+
 ?>
