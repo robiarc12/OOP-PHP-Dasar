9925204cc5d59527ce199c59d4884bebb6461c27
robiarc12 <robifirmansyah096@gmail.com>
2025-05-07 20:24:59 +0800

    Mengubah gaya pengodingan menggunakan constructor

--- produk.php
+++ produk.php
@@ -8,6 +8,15 @@ class Produk
     $harga = 0;
 
 
+    public function __construct($judul, $penulis, $penerbit, $harga)
+    {
+        echo "Judul: " . $this->judul = $judul;
+        echo "Penulis: " . $this->penulis = $penulis;
+        echo "Penerbit: " . $this->penerbit = $penerbit;
+        echo "Harga: " . $this->harga = $harga;
+        echo "<br>";
+
+    }
     public function sayHello()
     {
         return "hello World";
@@ -17,14 +26,16 @@ class Produk
 
 
 // ini bagaimana cara kita memanggil class yang sudah kita buat
-$produk1 = new Produk();
+$produk1 = new Produk("Ill Principe ", "Niccolo Machiaveli ", "Gramedia ", 73000);
 
-// cara memanggil dan menimpa isi default pada sebuah class
-$produk1->judul = "ill Principe";
-$produk1->penulis = "Machieveli";
+// cara memanggil dan menimpa isi default pada sebuah class tanpa constructor
+// $produk1->judul = "ill Principe";
+// $produk1->penulis = "Machieveli";
 
-$produk2 = new Produk();
+// membuat properti baru tanpa menginstansiasi di dalam class
+$produk2 = new Produk("Makannya Mikir!", "abigail & Cannia", "gramedia", 135000);
 $produk2->statusPembelian = "Pre-Order";
+var_dump($produk1);
 var_dump($produk2);
 
 
